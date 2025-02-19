4cad18fd07275d0e4e4b3b76562bf0b9d8be986c
DpkShinde <deepak.shinde@financeshastra.com>
2025-02-19 12:18:04 +0530

    updates

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -5,7 +5,7 @@ const WorkoutDetails = ({workout}) => {
     const {dispatch} = useWorkoutContext()
 
     const handleClick= async () => {
-        const response = await fetch('https://workout-buddy-backend-0u0y.onrender.com/api/workouts/' + workout._id, {
+        const response = await fetch('http://localhost:4000/api/workouts/' + workout._id, {
             method:'DELETE'
         })
         const json = await response.json()
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts');
+            const response = await fetch('https://workout-buddy-backend-0u0y.onrender.com/api/workouts');
             const json = await response.json();
             if (response.ok){
                 dispatch({type: 'SET_WORKOUTS', payload: json})
